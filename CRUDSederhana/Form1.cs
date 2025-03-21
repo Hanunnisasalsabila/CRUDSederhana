75856b4a1b6fb972ee1235ad84c29d431623bc47
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 17:07:08 +0700

    Mengosongkan semua input di TextBox setelah data dihapus

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -120,7 +120,7 @@ namespace CRUDSederhana
                                 {
                                     MessageBox.Show("Data berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
                                     LoadData();
-
+                                    ClearForm();
                                 }
                             }
                         }
