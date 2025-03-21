b5d4a9f60a8fcf5217ebe4a777c7825cb85b2cbe
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 16:06:20 +0700

    membuat objek DataTable yang akan digunakan untuk menyimpan data hasil query dari database

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -42,6 +42,7 @@ namespace CRUDSederhana
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
 
                 }
 
