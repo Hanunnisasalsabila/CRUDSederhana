362d23ebfc1450def1baae1d8e37b6fc6f62288b
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 17:27:55 +0700

    Jika pengguna menekan tombol "Yes" pada kotak dialog konfirmasi, maka proses penghapusan data akan dilanjutkan

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -145,6 +145,10 @@ namespace CRUDSederhana
             if (dgvMahasiswa.SelectedRows.Count > 0)
             {
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (confirm == DialogResult.Yes)
+                {
+
+                }
             }
         }
 
