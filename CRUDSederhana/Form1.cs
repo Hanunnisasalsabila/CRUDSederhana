81fa4fedb0d688c0d587db716b256784f9d7ff46
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 17:32:22 +0700

    Membuat objek SqlCommand untuk mengeksekusi perintah SQL DELETE pada koneksi database yang sedang terbuka

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -154,6 +154,10 @@ namespace CRUDSederhana
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
                             conn.Open();
                             string query = "DELETE FROM Mahasiswa WHERE NIM = @NIM;";
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+
+                            }
                         }
                     }
                 }
