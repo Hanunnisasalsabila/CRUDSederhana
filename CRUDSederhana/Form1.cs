1e67313e6552eec4c35380779f6ed3e5e71442f5
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 17:31:04 +0700

    Membuka koneksi ke database agar bisa menjalankan perintah SQL

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -152,6 +152,7 @@ namespace CRUDSederhana
                         try
                         {
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
+                            conn.Open();
                         }
                     }
                 }
