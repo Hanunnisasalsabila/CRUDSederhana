2a14019e377f717a3e134c1725ba5b6faf965c90
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 15:24:19 +0700

    buat terhubung ke database SQL Server untuk mengambil atau mengubah data

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -12,7 +12,7 @@ namespace CRUDSederhana
 {
     public partial class Form1 : Form
     {
-
+        private string connectionString = "Data Source=LAPTOP-DKIM0LL5\\HANUNNISA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
