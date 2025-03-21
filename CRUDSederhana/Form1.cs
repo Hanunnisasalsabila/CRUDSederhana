bdf3d1388bd3c8157f69925d374de5a61019d8b5
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-03-21 16:49:30 +0700

    Mengosongkan semua input di form agar siap digunakan untuk memasukkan data baru

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -81,7 +81,7 @@ namespace CRUDSederhana
                         {
                             MessageBox.Show("Data berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
                             LoadData();
-
+                            ClearForm();
                         }
                     }
                 }
